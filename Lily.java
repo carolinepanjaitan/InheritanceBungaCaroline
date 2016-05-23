c432a264df3d4b7cb8fdfa001c7fa59105171a8f
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-05-23 20:37:35 +0700

    latihan

---
+++ Lily.java
@@ -0,0 +1,20 @@
+package com.polymorphism.isa;
+
+public class Lily extends Bunga {
+
+    @Override
+    public void mati() {
+        super.mati(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void tumbuh() {
+        super.tumbuh(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mekar() {
+        super.mekar(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
